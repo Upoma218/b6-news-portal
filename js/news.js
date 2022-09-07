3d75ba82c075112f8f201eb5fd562ef64286fe02
Resma Islam Upoma <upoma.cdsr@gmail.com>
2022-09-07 15:53:48 +0600

    card section added

--- js/news.js
+++ js/news.js
@@ -19,26 +19,23 @@ const setAllMenu = async() =>{
         <a class="nav-link active me-3 ms-2 fw-semibold text-secondary" aria-current="page" href="#">${category.category_name}</a>
         `;
         categoryList.appendChild(li);
-        console.log(categoryList)
     }
 }
 
-const displayNewsDetails = document.getElementById('news-card');
-const newsDiv = document.createElement('div');
-newsDiv.classList.add('row g-0');
-newsDiv.innerHTML = `
-<div class="col-md-4">
-<img src="..." class="img-fluid rounded p-3" alt="...">
-</div>
-<div class="col-md-8">
-<div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-</div>
-</div>
-`;
-displayNewsDetails.appendChild(newsDiv);
+const newsCategories = async(_id) => {
+const url = `https://openapi.programming-hero.com/api/news/$`
+    const res = await fetch(url);
+    const data = await res.json();
+    const allNewsData = data;
+    console.log(allNewsData)
+    return allNewsData;
+    ;
+    
+}
+newsCategories();
+
+
+
 
 
 setAllMenu();
